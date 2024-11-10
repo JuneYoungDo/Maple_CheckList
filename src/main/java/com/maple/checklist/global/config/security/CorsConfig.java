cd0b9602c424ec7cf96b9540f59afb7d33873589
JuneYoungDo <ehwnsdud1004@naver.com>
2024-11-10 20:13:57 +0900

    cors 수정

--- src/main/java/com/maple/checklist/global/config/security/CorsConfig.java
+++ src/main/java/com/maple/checklist/global/config/security/CorsConfig.java
@@ -13,9 +13,9 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);   // 내 서버가 응답을 할 때 json 을 자바스크립트에서 처리할 수 있게 할지 설정
+//        config.setAllowCredentials(true);   // 내 서버가 응답을 할 때 json 을 자바스크립트에서 처리할 수 있게 할지 설정
         config.addAllowedOrigin("*");   // 모든 ip에 응답을 허용하겠다
-        config.addAllowedOrigin("http://localhost:3000");
+//        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");   // 모든 헤더에 응답을 허용하겠다
         config.addAllowedMethod("*");   // 모든 method 요청에 허용하겠다
         source.registerCorsConfiguration("/api/**",config);
