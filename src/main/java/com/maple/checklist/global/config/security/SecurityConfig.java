67df94ea6540c1d020dcbb2bd20a399d5b6e0006
Tory <tory@Toryui-MacBookAir.local>
2024-11-11 13:38:04 +0900

    remove cors for test

--- src/main/java/com/maple/checklist/global/config/security/SecurityConfig.java
+++ src/main/java/com/maple/checklist/global/config/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
+            // .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
             .addFilterBefore(
                 new JwtAuthenticationFilter(jwtTokenProvider),
                 UsernamePasswordAuthenticationFilter.class
