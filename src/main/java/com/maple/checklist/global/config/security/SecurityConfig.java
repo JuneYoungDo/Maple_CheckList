43585b839d04f49cb0aa76a4e8b592c40bc9550f
JuneYoungDo <ehwnsdud1004@naver.com>
2024-11-13 21:52:19 +0900

    테스트용 cors 수정

--- src/main/java/com/maple/checklist/global/config/security/SecurityConfig.java
+++ src/main/java/com/maple/checklist/global/config/security/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
         configuration.addAllowedOrigin("https://quest-book.store");
         configuration.addAllowedOrigin("https://eul97.github.io");
+        configuration.addAllowedOrigin("localhost:3000");
         configuration.addAllowedOrigin("192.168.0.7");
         configuration.addAllowedHeader("*"); // 모든 Header 허용
         configuration.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
