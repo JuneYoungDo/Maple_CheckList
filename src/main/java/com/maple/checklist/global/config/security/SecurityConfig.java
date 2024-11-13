2aca4a5ac7403dddd1e8bda0ddb0c6d3cf7e1f46
JuneYoungDo <ehwnsdud1004@naver.com>
2024-11-13 22:05:18 +0900

    테스트용 cors 수정

--- src/main/java/com/maple/checklist/global/config/security/SecurityConfig.java
+++ src/main/java/com/maple/checklist/global/config/security/SecurityConfig.java
@@ -67,11 +67,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("*");
-//        configuration.addAllowedOrigin("https://quest-book.store");
-//        configuration.addAllowedOrigin("https://eul97.github.io");
-//        configuration.addAllowedOrigin("localhost:3000");
-//        configuration.addAllowedOrigin("192.168.0.7");
+        configuration.addAllowedOrigin("https://quest-book.store");
+        configuration.addAllowedOrigin("https://eul97.github.io");
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://192.168.0.7");
+        configuration.addAllowedOrigin("https://192.168.0.7:3000");
         configuration.addAllowedHeader("*"); // 모든 Header 허용
         configuration.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
